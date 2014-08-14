1d4de78f02aee5c1f774bdb44ceee7980218cdf9
TheRedBerrys <theredberrys@gmail.com>
2014-08-14 11:05:01 -0400

    Initial code commit.

---
+++ PegGameJS/Ruleset.js
@@ -0,0 +1,35 @@
+function OriginalRuleset()
+{
+	this.description = "One Lower";
+	
+	this.canMove = function(index, current)
+	{
+        for (i = 1; i < index - 1; i++)
+			if (current[i])
+				return false;
+
+		if (!current[index - 1])
+			return false;
+
+		return true;
+	}
+}
+
+function FactorRuleset()
+{
+	this.description = "Factors";
+	
+	this.canMove = function(index, current)
+	{
+		for (i = 1; i < index; i++)
+			if ((index % i == 0) !== current[i])
+				return false;
+
+		return true;
+	}
+}
+
+function Rulesets()
+{
+	return [new OriginalRuleset(), new FactorRuleset()];
+}
