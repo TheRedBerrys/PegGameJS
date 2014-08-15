294e5a13aef5ae046c349fba345e4b32855df065
Spencer Robinson <theredberrys@gmail.com>
2014-08-15 00:28:25 -0400

    UI work.

--- PegGameJS/GameManager.js
+++ PegGameJS/GameManager.js
@@ -1,14 +1,31 @@
 var GameManager = GameManager || {};
 
 GameManager.rulesets = Rulesets();
+GameManager.ruleset = GameManager.rulesets[0];
 
 GameManager.startNewGame = function() {
-	this.ruleset = this.rulesets[0];
+	this.ruleset = this.rulesets[Math.floor(Math.random() * (this.rulesets.length))];
 
 	this.targetPegs = [true];
 	this.currentPegs = [true];
 	for (i = 1; i <= 8; i++) {
 		this.targetPegs.push(Math.random()<.5);
 		this.currentPegs.push(Math.random()<.5);
+	}
+}
+
+GameManager.startFirstGame = function() {
+	this.ruleset = this.rulesets[0];
+	
+	this.currentPegs = [true, false, false, false, false, false, false, false, false];
+	this.targetPegs = [true];
+	for (i = 1; i <= 8; i++) {
+		this.targetPegs.push(Math.random()<.5);
+	}
+}
+
+GameManager.trySwitch = function(index) {
+	if (this.ruleset.canMove(index, this.currentPegs)) {
+		this.currentPegs[index] = !this.currentPegs[index];
 	}
 }
